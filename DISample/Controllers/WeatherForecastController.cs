22e7f0274a41f3d6ba966bc716606caab6eb76e4
Sasa M. Vasiljevic <Sasa-M.Vasiljevic@comtrade.com>
2020-09-12 13:04:50 +0200

    Add project files.

---
+++ DISample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace DISample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWheatherForecastService _wheatherForecastService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWheatherForecastService wheatherForecastService)
+        {
+            _logger = logger;
+            _wheatherForecastService = wheatherForecastService;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _wheatherForecastService.GetForecast();
+        }
+    }
+}
